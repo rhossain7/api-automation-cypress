95651bffe1fb8ed637d3bda91784aa4bb1b7a5d3
rhossain7 <reedwan.hossan@rootnext.com>
2023-09-22 15:38:31 +0600

    removed unwanted file

--- cypress/e2e/spec.cy.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress" />
-
-it('google test', () => {
-  cy.visit('https://google.com')
-  cy.get('.SDkEP').type('Automation Step by Steps{enter}')
-  cy.get(':nth-child(4) > .g > .N54PNb > .jGGQ5e > .yuRUbf > :nth-child(1) > [jscontroller="msmzHf"] > a > .notranslate > :nth-child(2) > .byrV5b > .qLRx3b').click()
-})
