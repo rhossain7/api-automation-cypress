c4e0ea691ec0268e6411f6e6e3199c4593988e12
rhossain7 <reedwan.hossan@rootnext.com>
2023-09-22 20:14:27 +0600

    added Cypress dashboard projectID and env vars to GHA config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  projectId: "49eq7b",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
